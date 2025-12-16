47bf89390173296297dd9309587aa94b83ab1f17
Heidi <henbuska@hive.student.fi>
2025-12-16 21:43:49 +0200

    Change course and its parts into a a single JS object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -33,29 +33,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <h1>
-        <Header course={course} />
+        <Header course={course.name} />
       </h1>
-      <Content parts={parts}/>
-      <Total parts={parts}/> 
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/> 
     </div>
   )
 }
