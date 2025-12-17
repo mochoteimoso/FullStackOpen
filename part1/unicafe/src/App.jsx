d7a8b98855e3db52d62ad7c808917a4d599c907f
Heidi <henbuska@hive.student.fi>
2025-12-17 14:09:47 +0200

    Step 2 - add more statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,13 +10,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const [total, setTotal] = useState(0)
+  const [score, setScore] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    setScore(score + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+    setScore(score)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setScore(score - 1)
+  }
+
 
   return (
     <div>
       <h1> give feedback </h1>
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
       <h1> statistics </h1>
         <p>
           good {good}
@@ -24,6 +46,12 @@ const App = () => {
           neutral {neutral}
           <br/>
           bad {bad}
+           <br/>
+          all {total}
+           <br/>
+          average {score / total}
+           <br/>
+          positive {good / total * 100} %
         </p>
     </div>
   )
