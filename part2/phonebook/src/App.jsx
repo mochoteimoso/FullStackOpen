04ad36610f563052e08359885802085a2e880f91
Heidi <henbuska@hive.student.fi>
2025-12-18 14:53:35 +0200

    Phonebook step 4 - add a search field to filter the persons shown

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,10 +8,14 @@ const Person = ({ name, number }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,19 +39,34 @@ const App = () => {
     }
   }
 
+  const personsToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-   const handleNumberChange = (event) => {
+  const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+       filter shown with
+          <input
+            value={filter}
+            onChange={handleFilterChange}
+          />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -68,7 +87,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person =>
+        {personsToShow.map(person =>
             <Person key={person.name} name={person.name} number={person.number}/>
         )}
     </div>
