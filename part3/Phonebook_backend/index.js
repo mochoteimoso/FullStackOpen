a798df1e2e8491a099fd9769b7831bc2f5f1d667
Heidi <henbuska@hive.student.fi>
2025-12-26 17:23:51 +0200

    Phonebook backend  step 6 - Add error handling for new entry creation, i.e.  for missing name/number and duplicate name

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -39,6 +39,13 @@ app.post('/api/persons', (request, response) => {
       error: 'name or number missing'
   })
 
+  const duplicateName = persons.find(person => person.name === body.name)
+
+  if (duplicateName)
+     return response.status(400).json({
+      error: 'name must be unique'
+  })
+
   const person = {
     id: generateId(),
     name: body.name,
