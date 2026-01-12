553cef3eea6a019529e2c16c2864bef5d3d96107
Heidi <henbuska@hive.student.fi>
2026-01-12 11:38:31 +0200

    Full stack Phonebook - Generate a production build of frontend and add it to backend, create scripts for production build and copying, add proxy to vite.config, remove cors references

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 morgan.token('body', function getBody(req) {
   return JSON.stringify(req.body)
@@ -9,7 +8,7 @@ morgan.token('body', function getBody(req) {
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
