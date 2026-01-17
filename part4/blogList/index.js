87cc877423752e0b4701223558c72ecb7e205db9
Heidi <henbuska@hive.student.fi>
2026-01-17 17:54:11 +0200

    Blog List step 2 - Refactor app by separating different functions into their own files, tested with REST client and routes and database work

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,21 +1,6 @@
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
 
-const express = require('express')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
-
-const app = express()
-
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
