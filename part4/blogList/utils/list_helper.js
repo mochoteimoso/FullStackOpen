66688b1d7835590cf732cc33dcc3b5fa247da271
Heidi <henbuska@hive.student.fi>
2026-01-18 18:38:26 +0200

    Helper functions and unit tests step 3 - Define a favoriteBlog function and write tests for it

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -3,13 +3,25 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((sum, blog) => {
-    return sum + blog.likes
- }, 0)
+  return blogs.reduce((sum, blog) =>
+    sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  return blogs.reduce((favorite, blog) => 
+    blog.likes > favorite.likes
+     ? blog
+     : favorite
+  )
 }
 
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
