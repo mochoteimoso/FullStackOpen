a076d3f21752ac86bffcda5e7073655a41488343
Heidi <henbuska@hive.student.fi>
2026-01-19 15:44:08 +0200

    Blog List Tests step 1 - Install Supertest and edit scripts to include mode in order to use separate database for testing, move shared testing resources into a helper file, add tests and edit get route hander to use asynch/await

--- part4/blogList/tests/total_likes.test.js
+++ part4/blogList/tests/total_likes.test.js
@@ -1,88 +1,25 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
-
-const emptyList = []
-
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0
-  }
-]
-
-const listWithMultipleBlogs = [
-  {
-   _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }
-]
+const helper = require('./test_helper')
 
 describe('total likes', () => {
 
    test('when list is empty, result should be zero', () => {
-    const result = listHelper.totalLikes(emptyList)
-    console.log("Total likes Test 1: sum = ", result)
+    const result = listHelper.totalLikes(helper.emptyList)
+    //console.log("Total likes Test 1: sum = ", result)
     assert.strictEqual(result, 0)
   })
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    console.log("Total likes Test 2: sum = ", result)
+    const result = listHelper.totalLikes(helper.listWithOneBlog)
+    //console.log("Total likes Test 2: sum = ", result)
     assert.strictEqual(result, 5)
   })
 
   test('when list has multiple blogs, equals the sum of their likes', () => {
-    const result = listHelper.totalLikes(listWithMultipleBlogs)
-    console.log("Total likes Test 3: sum = ", result)
+    const result = listHelper.totalLikes(helper.listWithMultipleBlogs)
+    //console.log("Total likes Test 3: sum = ", result)
     assert.strictEqual(result, 36)
   })
 })
@@ -90,20 +27,20 @@ describe('total likes', () => {
 describe('most likes', () => {
 
   test('when list is empty, no blog should be returned', () => {
-    const result = listHelper.favoriteBlog(emptyList)
-    console.log("Most likes Test 1: = ", result)
+    const result = listHelper.favoriteBlog(helper.emptyList)
+    //console.log("Most likes Test 1: = ", result)
     assert.deepStrictEqual(result, null)
   })
 
   test('when list has only one blog, equals that one', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog)
-    console.log("Most likes Test 2: = ", result)
-    assert.strictEqual(result, listWithOneBlog[0])
+    const result = listHelper.favoriteBlog(helper.listWithOneBlog)
+    //console.log("Most likes Test 2: = ", result)
+    assert.strictEqual(result, helper.listWithOneBlog[0])
   })
 
   test('when list has multiple blogs, equals the one with most likes', () => {
-    const result = listHelper.favoriteBlog(listWithMultipleBlogs)
-    console.log("Most likes Test 3: = ", result)
-    assert.strictEqual(result, listWithMultipleBlogs[2])
+    const result = listHelper.favoriteBlog(helper.listWithMultipleBlogs)
+    //console.log("Most likes Test 3: = ", result)
+    assert.strictEqual(result, helper.listWithMultipleBlogs[2])
   })
 })
