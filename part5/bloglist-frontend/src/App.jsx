d1bf929d481523bb4f742a763770f2d14179ef56
Heidi <henbuska@hive.student.fi>
2026-01-22 18:41:37 +0200

    Blog List Frontend step 2 - Save login details into local storage and implement logout functionality that clears data from local storage

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -15,22 +16,39 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogListUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
 
+      window.localStorage.setItem(
+        'loggedBlogListUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch {
-      setErrorMessage('wrong credentials')
+      /*setErrorMessage('wrong credentials')*/
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
     }
   }
+
+  const handleLogout = () => {
+      window.localStorage.removeItem('loggedBlogListUser')
+      setUser(null)
+  }
+
 
   if (user === null) {
     return (
@@ -66,8 +84,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <div style={{ marginBottom: '1rem'}}>
-        {user.username} logged in
+      <div>
+        <p>{user.name} logged in
+           <button onClick={handleLogout}>logout</button>
+        </p>
       </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
